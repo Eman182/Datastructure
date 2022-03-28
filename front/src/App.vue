4654736d597f9a41101b4e1a0ceb59316805f768
Eman182 <61158961+Eman182@users.noreply.github.com>
2022-03-28 22:05:15 +0200

    Add files via upload

---
+++ front/src/App.vue
@@ -0,0 +1,322 @@
+<template>
+  <div id="app">
+    <head>
+      <title>Calculator</title>
+    </head>
+    <body>
+      <div class="main">
+        <div class="form">
+          <input type="text" name="textview" class="textview" v-model="text1">
+        </div>
+        <div class="clearfix">
+          <div class="col-3"><input type="button" value="%" class="button" v-on:click="ConcatForPercent();"></div>
+          <div class="col-3"><input type="button" value="CE" class="button" v-on:click="Clearing();"></div>
+          <div class="col-3"><input type="button" value="C" class="button" v-on:click="Clearing();"></div>
+          <div class="col-3"><input type="button" value="&#9003;" class="button" v-on:click="ClearLast();"></div>
+        </div>
+        <div class="clearfix">
+          <div class="col-3"><input type="button" value="1/x" class="button" v-on:click="ConcatForMultInverse();"></div>
+          <div class="col-3"><input type="button" value="x&#x00B2;" class="button" v-on:click="ConcatForSquaring();"></div>
+          <div class="col-3"><input type="button" value="&#x221A;" class="button" v-on:click="ConcatForSquareRoot();"></div>
+          <div class="col-3"><input type="button" value="&#xf7;" class="button" v-on:click="insert('/');" ></div>
+        </div>
+        <div class="clearfix">
+          <div class="col-3"><input type="button" value="7" class="button" v-on:click="insert('7');"></div>
+          <div class="col-3"><input type="button" value="8" class="button" v-on:click="insert('8');"></div>
+          <div class="col-3"><input type="button" value="9" class="button" v-on:click="insert('9');"></div>
+          <div class="col-3"><input type="button" value="&#xd7;" class="button" v-on:click="insert('*')"></div>
+        </div>
+        <div class="clearfix">
+          <div class="col-3"><input type="button" value="4" class="button" v-on:click="insert('4');"></div>
+          <div class="col-3"><input type="button" value="5" class="button" v-on:click="insert('5');"></div>
+          <div class="col-3"><input type="button" value="6" class="button" v-on:click="insert('6');"></div>
+          <div class="col-3"><input type="button" value="-" class="button" v-on:click="insert('-');"></div>
+        </div>
+        <div class="clearfix">
+          <div class="col-3"><input type="button" value="1" class="button" v-on:click ="insert('1');"></div>
+          <div class="col-3"><input type="button" value="2" class="button" v-on:click ="insert('2');"></div>
+          <div class="col-3"><input type="button" value="3" class="button" v-on:click="insert('3');"></div>
+          <div class="col-3"><input type="button" value="+" class="button" v-on:click="insert('+');"></div>
+        </div>
+        <div class="clearfix">
+          <div class="col-3"><input type="button" value="+/-" class="button" v-on:click="Negation();"></div>
+          <div class="col-3"><input type="button" value="0" class="button" v-on:click="insert('0');"></div>
+          <div class="col-3"><input type="button" value="." class="button" v-on:click="insert('.');"></div>
+          <div class="col-3"><input type="button" value="=" class="button" v-on:click ="calc();"></div>
+        </div>
+      </div>
+    </body>
+  </div>
+
+</template>
+
+<style >
+  #app{
+    margin : 0;
+    padding : 0;
+    box-sizing: border-box;
+  }
+    body{
+      background: #333 ; 
+    }
+    .main{
+      background: yellow;
+      text-align: center;
+      width: 400px;
+      margin: 80px auto;
+      padding: 20px;
+    }
+    .textview{
+      width: 100%;font-size: 20px;
+			height: 40px;margin: -10px;
+			padding:0 5px;
+			pointer-events: none;
+    }
+    .clearfix:before,.clearfix:after{
+      content: ' ';margin :3px;
+      display: table;
+       clear:both; 
+      width: 100%;
+    }
+    .col-3{
+      width: 20%;padding: 5px;
+      float: left;margin: 5px
+    }
+    .button{
+      width: 100%;font-size: 22px;
+      height: 40px;padding: 0 10px;
+      outline: none;background: red;
+      size: 500px
+    }
+</style>
+<script>
+import axios from 'axios'
+export default {
+  el: '#app',
+  data:function() {
+    return {
+    text1 : ''
+    };
+  },
+  methods :{
+   insert : function(num) {
+     var str=String(this.text1);
+    var lastChar=str.charAt(str.length-1);
+    if(str.length < 35){
+      if(lastChar==null||(num>='0'&&num<='9')||num=='.'||(num!=lastChar&&(lastChar>='0'&&lastChar<='9'))
+      ||(lastChar==')')){
+        str=str.concat(num);this.text1=str;
+      }
+      else if(!(num>='0'&&num<='9')&&num!=lastChar&&!(lastChar>='0'&&lastChar<='9')){
+        var str1=str.substring(0,str.length-1);
+        if(lastChar!='.'){
+        this.text1=str1.concat(num);
+        }else{
+          this.text1=String(this.text1).concat(num);
+        }
+      }else{
+        this.text1=this.text1+"";
+      }
+    }
+  },
+   ConcatForPercent :function(){
+    var str=String(this.text1);
+      var i=str.length-1;
+      if(str.charAt(str.length-1)==')'){
+        //loop to find '('
+        
+        while(str.charAt(i)!='('){
+          i--;
+        }
+        str=str.substr(0,i+1)+'(('+str.substr(i+1,str.length)+")/100)"
+        this.text1=str;
+      }else{
+        //it must be number or . if the format is true  then we want to reach the character at which we will put
+        //the paranthess
+        
+        if(str.charAt(i)=='.'||(str.charAt(i)<='9'&&str.charAt(i)>='0')){
+          while ((str.charAt(i)>='0'&&str.charAt(i)<='9')||(str.charAt(i)=='.')){
+             i--;
+           }
+           //now we reached the character we want to put paranthess after it 
+          
+            str=str.substr(0,i+1)+'(('+str.substr(i+1,str.length)+')/100)';
+            this.text1=str;
+           
+        } else{
+          //it is wrong format the backend will handle it so only put the paranthess before and after
+           this.text1='(('+String(this.text1).concat(")/100)");
+        }
+      }
+  },
+   Negation:function(){
+    if( String(this.text1).length<31){
+      var str=String(this.text1);
+      var i=str.length-1;
+      if(str.charAt(str.length-1)==')'){
+        //loop to find '('
+        
+        while(str.charAt(i)!='('){
+          i--;
+        }
+        str=str.substr(0,i+1)+'(('+str.substr(i+1,str.length)+")*-1)"
+        this.text1=str;
+      }else{
+        //it must be number or . if the format is true  then we want to reach the character at which we will put
+        //the paranthess
+        
+        if(str.charAt(i)=='.'||(str.charAt(i)<='9'&&str.charAt(i)>='0')){
+          while ((str.charAt(i)>='0'&&str.charAt(i)<='9')||(str.charAt(i)=='.')){
+             i--;
+           }
+           //now we reached the character we want to put paranthess after it 
+          
+            str=str.substr(0,i+1)+'(('+str.substr(i+1,str.length)+')*-1)';
+            this.text1=str;
+           
+        } else{
+          //it is wrong format the backend will handle it so only put the paranthess before and after
+           this.text1='(('+String(this.text1).concat(")*-1)");
+        }
+      }
+    }
+  },
+   ConcatForSquareRoot:function(){
+    if( String(this.text1).length<34){
+      var str=String(this.text1);
+      var i=str.length-1;
+      if(str.charAt(str.length-1)==')'){
+        //loop to find '('
+        
+        while(str.charAt(i)!='('){
+          i--;
+        }
+        str=str.substr(0,i+1)+'(('+str.substr(i+1,str.length)+")^0.5)"
+        this.text1=str;
+      }else{
+        //it must be number or . if the format is true  then we want to reach the character at which we will put
+        //the paranthess
+        
+        if(str.charAt(i)=='.'||(str.charAt(i)<='9'&&str.charAt(i)>='0')){
+          while ((str.charAt(i)>='0'&&str.charAt(i)<='9')||(str.charAt(i)=='.')){
+             i--;
+           }
+           //now we reached the character we want to put paranthess after it 
+          
+            str=str.substr(0,i+1)+'(('+str.substr(i+1,str.length)+')^0.5)';
+            this.text1=str;
+           
+        } else{
+          //it is wrong format the backend will handle it so only put the paranthess before and after
+           this.text1='(('+String(this.text1).concat(")^0.5)");
+        }
+      }
+    }
+  },
+   ConcatForMultInverse:function(){
+    var str=String(this.text1);
+      var i=str.length-1;
+      if(str.charAt(str.length-1)==')'){
+        //loop to find '('
+        
+        while(str.charAt(i)!='('){
+          i--;
+        }
+        str=str.substr(0,i+1)+'(('+str.substr(i+1,str.length)+")^-1)"
+        this.text1=str;
+      }else{
+        //it must be number or . if the format is true  then we want to reach the character at which we will put
+        //the paranthess
+        
+        if(str.charAt(i)=='.'||(str.charAt(i)<='9'&&str.charAt(i)>='0')){
+          while ((str.charAt(i)>='0'&&str.charAt(i)<='9')||(str.charAt(i)=='.')){
+             i--;
+           }
+           //now we reached the character we want to put paranthess after it 
+          
+            str=str.substr(0,i+1)+'(('+str.substr(i+1,str.length)+')^-1)';
+            this.text1=str;
+           
+        } else{
+          //it is wrong format the backend will handle it so only put the paranthess before and after
+           this.text1='(('+String(this.text1).concat(")^-1)");
+        }
+      }
+  },
+   ConcatForSquaring :function(){
+    if(String(this.text1).length<35){
+      var str=String(this.text1);
+      var i=str.length-1;
+      if(str.charAt(str.length-1)==')'){
+        //loop to find '('
+        
+        while(str.charAt(i)!='('){
+          i--;
+        }
+        str=str.substr(0,i+1)+'(('+str.substr(i+1,str.length)+")^2)"
+        this.text1=str;
+      }else{
+        //it must be number or . if the format is true  then we want to reach the character at which we will put
+        //the paranthess
+        
+        if(str.charAt(i)=='.'||(str.charAt(i)<='9'&&str.charAt(i)>='0')){
+          while ((str.charAt(i)>='0'&&str.charAt(i)<='9')||(str.charAt(i)=='.')){
+             i--;
+           }
+           //now we reached the character we want to put paranthess after it 
+          
+            str=str.substr(0,i+1)+'(('+str.substr(i+1,str.length)+')^2)';
+            this.text1=str;
+           
+        } else{
+          //it is wrong format the backend will handle it so only put the paranthess before and after
+           this.text1='(('+String(this.text1).concat(")^2)");
+        }
+      }
+    }
+  },
+  Clearing :function(){
+    this.text1="";
+  },
+  ClearLast:function(){
+    if(String(this.text1).length>='1'){
+    this.text1=String(this.text1).substring(0,String(this.text1).length-1);
+    }//else we willnot delete as the string is empty
+  },
+  calc : function(){//get the result
+      var st=this.text1;
+      if(st.length==0){
+        this.text1="";
+      }else if((st.length==1&&st.charAt(0)=='.')){
+        this.text1="0";
+      }
+      else{
+       axios.get("http://localhost:8088/evaluate",{
+       params : {
+         expression : String(st),
+       }
+     })
+     .then(response=>{
+       console.log("The result is "+response.data);
+       if(String(response.data).length<35){
+       this.text1=response.data;
+       }else{
+         //if(String(response.data).contains(".")==false){
+         //var x=String(response.data).length-1;
+         //this.text1=String(response.data).charAt(0)+"."+String(response.data).substring(1,28)+"*"+"10^"+x;
+         //the length of result shouldn't be greater than display screen
+        // }else{
+             //we take substring directly
+             this.text1=String(response.data).substring(0,33)+"E";
+        // }
+       }
+     })
+      }
+  }
+  ,
+
+  },
+}
+</script>
+
+
